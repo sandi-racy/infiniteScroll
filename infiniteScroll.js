221557829427346ec3247dd7a1806e52f70bca36
sandi-racy <ndi_racy@rocketmail.com>
2013-08-01 18:40:55 +0700

    First Commit

--- infiniteScroll.js
+++ infiniteScroll.js
@@ -4,8 +4,8 @@
  * Copyright (c) 2013, Sandi Rosyandi
  */
  
-var success = true;
-jQuery.fn.infiniteScroll = function(options){
+jQuery.fn.infiniteScroll = function(status, options){
+	var tempOption = ($.type(status) == 'string' ? options : status);
 	iSOptions = jQuery.extend({
 		container: this,
 		classNextSelector: 'url:last',
@@ -13,13 +13,15 @@ jQuery.fn.infiniteScroll = function(options){
 		resultWord: 'done',
 		loading: '<div class="loading" style="text-align:center;">Loading...</div>',
 		classLoading: 'loading',
+		startAt: 1,
 		animation: 'fade',
 		animationSpeed: 'slow',
 		done: '<div style="color:#f00; text-align:center;">It\'s done.</div>'
-	}, options);
-	
+	}, tempOption);
+	var success = (status == 'disable' ? false : true);
 	$(window).scroll(function(){
-		if(($(window).scrollTop() == ($(document).height() - $(window).height())) && (success)){
+		var startAt = iSOptions.startAt * ($(document).height() - $(window).height());
+		if(($(window).scrollTop() >= startAt) && (success)){
 			success = false;
 			$("."+iSOptions.classNextSelector).after(iSOptions.loading);
 			var url = $("."+iSOptions.classNextSelector).data(iSOptions.dataclassNextSelector);
@@ -28,15 +30,14 @@ jQuery.fn.infiniteScroll = function(options){
 				url:url,
 				success:function(result){
 					if(result==iSOptions.resultWord){
-						var typeDone = $.type(iSOptions.done);
-						if(typeDone == 'string'){
+						if($.type(iSOptions.done) == 'string'){
 							var result = $(iSOptions.done).hide();
 							if(iSOptions.animation == 'fade')
 								result.appendTo($(iSOptions.container)).fadeIn(iSOptions.animationSpeed);
 							else
 								result.appendTo($(iSOptions.container)).slideDown(iSOptions.animationSpeed);							
 						}
-						else if(typeDone == 'function')
+						else if($.type(iSOptions.done) == 'function')
 							iSOptions.done();
 						success = false;
 					}
